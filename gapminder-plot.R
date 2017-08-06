7b51ac7a16a6362100ef0c9118b4af6e04bc7765
Ning Zhao <zhaoning@stanford.edu>
2017-08-06 11:00:31 -0700

    second commit

--- gapminder-plot.R
+++ gapminder-plot.R
@@ -1,4 +1,5 @@
 gapminder <- read.csv("gapminder-FiveYearData.csv")
 library(ggplot2)
-ggplot(gapminder_FiveYearData, aes(x = gdpPercap, y = lifeExp)) + geom_point(aes(color =continent))
-
+ggplot(gapminder_FiveYearData, aes(x = gdpPercap, y = lifeExp)) + geom_point(aes(color =continent)) + geom_line() + geom_smooth(method = "lm")
+d
+# here a new comment
